ba6ec312cc086888d0ef21e32c749ff531f9cc0c
Paul Jacobs <paul.jacobs@gmail.com>
2016-12-20 12:24:34 -0800

    Added ball movement script, got it working.

---
+++ Roll-A-Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	private Rigidbody rb;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveH = Input.GetAxis ("Horizontal");
+		float moveV = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveH, 0, moveV);
+
+		rb.AddForce (movement * speed);
+	}
+}
